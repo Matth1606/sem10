68bb2e95aaa593bbbc9cb2c44c13d3dfec9f4b11
Matth1606 <143737901+Matth1606@users.noreply.github.com>
2024-04-18 16:35:24 -0400

    fin

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const boutonsModale = document.querySelectorAll('.plus');
+const modale = document.querySelector('.modale');
+for (let i = 0; i < boutonsModale.length; i++) {
+    boutonsModale[i].addEventListener('click', openModale);
+}
+function openModale() {
+    modale.classList.add('visible');
+    modale.addEventListener("click", closeModale);
+}
+ 
+function closeModale(event) {
+    modale.classList.remove('visible');
+}
